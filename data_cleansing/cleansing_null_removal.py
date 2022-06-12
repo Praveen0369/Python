5e50b6cd3a7b14f4aae26e45de5d9e761df9d090
Praveen0369 <89630842+Praveen0369@users.noreply.github.com>
2022-06-12 19:56:04 +0530

    Create cleansing_null_removal.py

---
+++ data_cleansing/cleansing_null_removal.py
@@ -0,0 +1,38 @@
+import pandas as pd
+
+
+
+df=pd.read_csv("/content/clean_data.csv")
+
+df.head()
+
+df.info()
+
+#Trying To Clear Out Nulls Or Finding Nulls
+
+df.isna().sum().sum()
+
+df[df.columns[df.isna().any()]].isna().sum()
+
+df.isna().sum().plot(kind='hist')
+
+threshold = 800
+print(f"Columns with greater than {threshold} NaNs, {round((threshold/df.shape[0]) * 100)}% of it's values are NaNs.")
+
+outliers=df.isna().sum()[df.isna().sum()>threshold]
+
+outliers
+
+del df['bathroom']
+
+df.pop('age')
+
+df.isna().sum().plot(kind='hist')
+
+#checking for duplicates
+
+df.duplicated().sum()
+
+#output
+
+df.describe()
